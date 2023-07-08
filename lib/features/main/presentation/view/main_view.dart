ada49b1a2542975af51a2ac4a7f9ad7304ef7188
saleem-15 <94238476+saleem-15@users.noreply.github.com>
2023-07-09 00:30:21 +0300

    add border to navigation bar

--- lib/features/main/presentation/view/main_view.dart
+++ lib/features/main/presentation/view/main_view.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:nuntium/core/resorces/manager_colors.dart';
 import 'package:nuntium/core/resorces/manager_sizes.dart';
 import 'package:nuntium/core/widgets/will_pop_scope.dart';
 import 'package:persistent_bottom_nav_bar_v2/persistent-tab-view.dart';
@@ -22,13 +23,12 @@ class MainView extends StatelessWidget {
             confineInSafeArea: true,
             navBarHeight: ManagerHeight.h90,
             decoration: NavBarDecoration(
+              border: Border.all(
+                color: ManagerColors.greyLight.withOpacity(.32),
+              ),
               borderRadius: BorderRadius.only(
-                topLeft: Radius.circular(
-                  ManagerRadius.r16,
-                ),
-                topRight: Radius.circular(
-                  ManagerRadius.r16,
-                ),
+                topLeft: Radius.circular(ManagerRadius.r16),
+                topRight: Radius.circular(ManagerRadius.r16),
               ),
             ),
             screens: controller.screens,
