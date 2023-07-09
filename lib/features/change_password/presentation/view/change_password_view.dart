0102df320c2badd44e63405a19b89575ac8a62da
saleem-15 <94238476+saleem-15@users.noreply.github.com>
2023-07-09 15:45:42 +0300

    Refactor: ChangePasswordView

--- lib/features/change_password/presentation/view/change_password_view.dart
+++ lib/features/change_password/presentation/view/change_password_view.dart
@@ -11,70 +11,83 @@ import 'package:nuntium/core/widgets/rect_button.dart';
 import 'package:nuntium/core/widgets/text_field.dart';
 import 'package:nuntium/features/change_password/presentation/controller/change_password_controller.dart';
 
-class ChangePasswordView extends StatelessWidget {
+class ChangePasswordView extends GetView<ChangePasswordController> {
   final FieldValidator _validator = FieldValidator();
 
   ChangePasswordView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        leading: const BackButton(
-          color: ManagerColors.greyPrimary,
-        ),
-        title: Text(
-          ManagerStrings.termsAndConditions,
-          style: getSemiBoldTextStyle(
-            fontSize: ManagerFontSize.s24,
-            color: ManagerColors.blackPrimary,
+    return WillPopScope(
+      onWillPop: controller.onWillPop,
+      child: Scaffold(
+        appBar: AppBar(
+          leading: const BackButton(
+            color: ManagerColors.greyPrimary,
+          ),
+          title: Text(
+            ManagerStrings.changePassword,
+            style: getSemiBoldTextStyle(
+              fontSize: ManagerFontSize.s24,
+              color: ManagerColors.blackPrimary,
+            ),
           ),
         ),
-      ),
-      body: GetBuilder<ChangePasswordController>(
-        builder: (controller) => SingleChildScrollView(
-          child: Padding(
-            padding: EdgeInsets.symmetric(horizontal: ManagerWidth.w20),
-            child: Column(
-              children: [
-                myTextField(
-                  controller: controller.currentPasswordController,
-                  icon: ManagerIcons.password,
-                  hintText: 'Current Password',
-                  validator: (value) => _validator.validatePassword(value),
-                  keyboardType: TextInputType.text,
-                  isObscureText: true,
+        body: GetBuilder<ChangePasswordController>(
+          builder: (controller) => SingleChildScrollView(
+            child: Padding(
+              padding: EdgeInsets.symmetric(horizontal: ManagerWidth.w20),
+              child: Form(
+                key: controller.formKey,
+                child: Column(
+                  children: [
+                    SizedBox(
+                      height: ManagerHeight.h16,
+                    ),
+                    MyTextField(
+                      controller: controller.currentPasswordController,
+                      icon: ManagerIcons.password,
+                      hintText: 'Current Password',
+                      validator: (value) => _validator.validatePassword(value),
+                      keyboardType: TextInputType.visiblePassword,
+                      isObscureText: true,
+                    ),
+                    SizedBox(
+                      height: ManagerHeight.h16,
+                    ),
+                    MyTextField(
+                      controller: controller.newPasswordController,
+                      icon: ManagerIcons.password,
+                      hintText: ManagerStrings.newPasswordHint,
+                      validator: (value) => _validator.validatePassword(value),
+                      keyboardType: TextInputType.text,
+                      isObscureText: true,
+                    ),
+                    SizedBox(
+                      height: ManagerHeight.h16,
+                    ),
+                    MyTextField(
+                      controller: controller.repeatPasswordController,
+                      icon: ManagerIcons.password,
+                      hintText: ManagerStrings.repeatNewPassword,
+                      textInputAction: TextInputAction.done,
+                      validator: (value) => _validator.validatePasswordsMatch(
+                        controller.newPasswordController.text,
+                        value,
+                      ),
+                      keyboardType: TextInputType.text,
+                      isObscureText: true,
+                    ),
+                    SizedBox(
+                      height: ManagerHeight.h16,
+                    ),
+                    rectButton(
+                      onPressed: () => controller.performChangePassword(context),
+                      text: ManagerStrings.changePassword,
+                    ),
+                  ],
                 ),
-                SizedBox(
-                  height: ManagerHeight.h16,
-                ),
-                myTextField(
-                  controller: controller.currentPasswordController,
-                  icon: ManagerIcons.password,
-                  hintText: ManagerStrings.newPasswordHint,
-                  validator: (value) => _validator.validatePassword(value),
-                  keyboardType: TextInputType.text,
-                  isObscureText: true,
-                ),
-                SizedBox(
-                  height: ManagerHeight.h16,
-                ),
-                myTextField(
-                  controller: controller.currentPasswordController,
-                  icon: ManagerIcons.password,
-                  hintText: ManagerStrings.repeatNewPassword,
-                  validator: (value) => _validator.validatePassword(value),
-                  keyboardType: TextInputType.text,
-                  isObscureText: true,
-                ),
-                SizedBox(
-                  height: ManagerHeight.h16,
-                ),
-                rectButton(
-                  onPressed: () {},
-                  text: ManagerStrings.changePassword,
-                ),
-              ],
+              ),
             ),
           ),
         ),
