3bf09a09f5aabc60568d5b72289e137343b5ad8b
saleem-15 <94238476+saleem-15@users.noreply.github.com>
2023-07-09 15:41:45 +0300

    Update: add margin property for rectButton

--- lib/core/widgets/rect_button.dart
+++ lib/core/widgets/rect_button.dart
@@ -3,9 +3,14 @@ import 'package:nuntium/core/resorces/manager_colors.dart';
 import 'package:nuntium/core/resorces/manager_fonts.dart';
 import 'package:nuntium/core/resorces/manager_sizes.dart';
 
-Padding rectButton({required void Function() onPressed, required String text, bool isHasMargin = false}) {
+Padding rectButton({
+  required void Function() onPressed,
+  required String text,
+  EdgeInsets? margin,
+  bool isHasMargin = false,
+}) {
   return Padding(
-    padding: EdgeInsets.symmetric(horizontal: isHasMargin ? ManagerWidth.w20 : 0),
+    padding: margin ?? EdgeInsets.zero,
     child: ElevatedButton(
       onPressed: onPressed,
       style: ElevatedButton.styleFrom(
