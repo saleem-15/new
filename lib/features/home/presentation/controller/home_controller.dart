1bb3c6e96fb79aa2c960631da66c8daebeb8ed9e
saleem-15 <94238476+saleem-15@users.noreply.github.com>
2023-07-10 11:15:10 +0300

    added fetching articeles based on category

--- lib/features/home/presentation/controller/home_controller.dart
+++ lib/features/home/presentation/controller/home_controller.dart
@@ -1,3 +1,87 @@
+// import 'dart:developer';
+
+// import 'package:get/get.dart';
+// import 'package:infinite_scroll_pagination/infinite_scroll_pagination.dart';
+// import 'package:nuntium/config/constants.dart';
+// import 'package:nuntium/config/dependency_injection.dart';
+// import 'package:nuntium/features/home/domain/mapper/home_entity_mapper.dart';
+// import 'package:nuntium/features/home/presentation/model/category.dart';
+
+// import '../../domain/use_case/home_use_case.dart';
+// import '../model/article.dart';
+// import '../view/widgets/categories.dart';
+
+// class HomeController extends GetxController {
+//   final PagingController<int, Article> pagingController = PagingController(firstPageKey: 0);
+
+//   final _homeUseCase = instance<HomeUseCase>();
+
+//   int page = 1;
+//   late int totalResults;
+//   List<Article> articles = [];
+//   List<Category> categories = [];
+
+//   late String selctedCategory;
+
+//   @override
+//   void onInit() {
+//     super.onInit();
+//     selctedCategory = CategoriesEnum.values.first.name;
+//     categories = CategoriesEnum.values.map((e) => Category(name: e.name)).toList();
+
+//     pagingController.addPageRequestListener(_fetchPage);
+//   }
+
+//   Future<List<Article>> fetchArticles(int page) async {
+//     log('(fetchArticles) page: $page');
+//     return (await _homeUseCase.execute(
+//       HomeUseCaseInput(
+//         page: page,
+//         pageSize: ApiConstants.homeaPgeSizeValue,
+//         country: 'us',
+//       ),
+//     ))
+//         .fold(
+//       (l) {
+//         Get.rawSnackbar(message: l.message);
+//         return [];
+//       },
+//       (r) {
+//         totalResults = r.totalResults;
+//         return r.toPresentation().articles;
+//       },
+//     );
+//   }
+
+//   Future<void> _fetchPage(int pageKey) async {
+//     try {
+//       final newFetchedArticles = await fetchArticles(pageKey);
+//       final totalNumFetchedArticles = newFetchedArticles.length + (pagingController.itemList?.length ?? 0);
+
+//       final isLastPage = totalResults <= totalNumFetchedArticles;
+//       if (isLastPage) {
+//         pagingController.appendLastPage(newFetchedArticles);
+//       } else {
+//         final nextPageKey = page++;
+//         pagingController.appendPage(newFetchedArticles, nextPageKey);
+//       }
+//     } catch (error) {
+//       pagingController.error = error;
+//     }
+//   }
+
+//   @override
+//   void onClose() {
+//     pagingController.dispose();
+//     super.onClose();
+//   }
+
+//   void onCategoryPressed(String category) {
+//     selctedCategory = category;
+//     update();
+//   }
+// }
+
 import 'dart:developer';
 
 import 'package:get/get.dart';
@@ -5,9 +89,11 @@ import 'package:infinite_scroll_pagination/infinite_scroll_pagination.dart';
 import 'package:nuntium/config/constants.dart';
 import 'package:nuntium/config/dependency_injection.dart';
 import 'package:nuntium/features/home/domain/mapper/home_entity_mapper.dart';
+import 'package:nuntium/features/home/presentation/model/category.dart';
 
 import '../../domain/use_case/home_use_case.dart';
 import '../model/article.dart';
+import '../view/widgets/categories.dart';
 
 class HomeController extends GetxController {
   final PagingController<int, Article> pagingController = PagingController(firstPageKey: 0);
@@ -16,28 +102,28 @@ class HomeController extends GetxController {
 
   int page = 1;
   late int totalResults;
-
   List<Article> articles = [];
+  List<Category> categories = [];
+
+  late String selectedCategory;
 
   @override
   void onInit() {
     super.onInit();
+    categories = CategoriesEnum.values.map((e) => Category(name: e.name)).toList();
+    selectedCategory = categories.first.name;
+
     pagingController.addPageRequestListener(_fetchPage);
   }
 
-  bool changeColor({required bool isChecked}) {
-    isChecked = !isChecked;
-    update();
-    return isChecked;
-  }
-
-  Future<List<Article>> fetchArticles(int page) async {
-    log('(fetchArticles) page: $page');
+  Future<List<Article>> fetchArticles(int page, String category) async {
+    log('(fetchArticles) page: $page, category: $category');
     return (await _homeUseCase.execute(
       HomeUseCaseInput(
         page: page,
         pageSize: ApiConstants.homeaPgeSizeValue,
         country: 'us',
+        category: category,
       ),
     ))
         .fold(
@@ -54,7 +140,7 @@ class HomeController extends GetxController {
 
   Future<void> _fetchPage(int pageKey) async {
     try {
-      final newFetchedArticles = await fetchArticles(pageKey);
+      final newFetchedArticles = await fetchArticles(pageKey, selectedCategory);
       final totalNumFetchedArticles = newFetchedArticles.length + (pagingController.itemList?.length ?? 0);
 
       final isLastPage = totalResults <= totalNumFetchedArticles;
@@ -73,5 +159,14 @@ class HomeController extends GetxController {
   void onClose() {
     pagingController.dispose();
     super.onClose();
+  }
+
+  void onCategoryPressed(String category) {
+    selectedCategory = category;
+    // Reset the page and clear the existing articles when the category changes
+    page = 1;
+    articles.clear();
+    pagingController.refresh();
+    update();
   }
 }
