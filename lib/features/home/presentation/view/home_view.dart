b2dc64d9a7480b69a357c656aff2216cc56272f7
saleem-15 <94238476+saleem-15@users.noreply.github.com>
2023-07-13 13:22:48 +0300

    Update: pull to refresh & some refactoring

--- lib/features/home/presentation/view/home_view.dart
+++ lib/features/home/presentation/view/home_view.dart
@@ -1,18 +1,23 @@
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:infinite_scroll_pagination/infinite_scroll_pagination.dart';
-import 'package:nuntium/core/resorces/manager_colors.dart';
 import 'package:nuntium/core/resorces/manager_fonts.dart';
 import 'package:nuntium/core/resorces/manager_icons.dart';
 import 'package:nuntium/core/resorces/manager_sizes.dart';
 import 'package:nuntium/core/resorces/manager_strings.dart';
 import 'package:nuntium/core/resorces/manager_styles.dart';
+import 'package:nuntium/core/service/icon_service.dart';
 import 'package:nuntium/core/widgets/screen_header.dart';
+import 'package:nuntium/core/widgets/text_field.dart';
 import 'package:nuntium/features/home/presentation/controller/home_controller.dart';
 import 'package:nuntium/features/home/presentation/model/article.dart';
+import 'package:pull_to_refresh/pull_to_refresh.dart';
 
 import 'widgets/article_card.dart';
+import 'widgets/articles_loading_error.dart';
 import 'widgets/categories.dart';
+import 'widgets/articles_loading_indicator.dart';
 
 class HomeView extends GetView<HomeController> {
   const HomeView({super.key});
@@ -21,100 +26,88 @@ class HomeView extends GetView<HomeController> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: CustomScrollView(
-          slivers: [
-            ///Header
-            SliverPadding(
-              padding: EdgeInsets.symmetric(horizontal: ManagerWidth.w20),
-              sliver: SliverToBoxAdapter(
-                child: Header(
-                  title: ManagerStrings.browse,
-                  paragraph: ManagerStrings.homeParagraph,
+        child: SmartRefresher(
+          header: WaterDropMaterialHeader(offset: ManagerHeight.h230),
+          dragStartBehavior: DragStartBehavior.start,
+          controller: controller.refreshController,
+          onRefresh: controller.onRefresh,
+          child: CustomScrollView(
+            slivers: [
+              ///Header
+              SliverToBoxAdapter(
+                child: Padding(
+                  padding: EdgeInsets.symmetric(horizontal: ManagerWidth.w20),
+                  child: Header(
+                    title: ManagerStrings.browse,
+                    paragraph: ManagerStrings.homeParagraph,
+                  ),
                 ),
               ),
-            ),
 
-            ///search field
-            SliverPadding(
-              padding: EdgeInsets.symmetric(horizontal: ManagerWidth.w20),
-              sliver: SliverToBoxAdapter(
-                child: Container(
-                  height: ManagerHeight.h56,
-                  decoration: BoxDecoration(
-                    color: ManagerColors.greyLighter,
-                    borderRadius: BorderRadius.all(
-                      Radius.circular(ManagerRadius.r16),
+              ///search field
+              SliverToBoxAdapter(
+                child: Padding(
+                  padding: EdgeInsets.symmetric(horizontal: ManagerWidth.w20),
+                  child: MyTextField(
+                    controller: TextEditingController(),
+                    textInputAction: TextInputAction.search,
+                    hintText: ManagerStrings.search,
+                    icon: IconService.getIcon(
+                      icon: ManagerIcons.search,
                     ),
-                  ),
-                  child: TextField(
-                    decoration: InputDecoration(
-                      contentPadding: EdgeInsetsDirectional.symmetric(
-                        vertical: ManagerHeight.h16,
-                      ),
-                      hintText: ManagerStrings.search,
-                      hintStyle: getMediumTextStyle(
-                        fontSize: ManagerFontSize.s16,
-                        color: ManagerColors.greyPrimary,
-                      ),
-                      border: InputBorder.none,
-                      prefixIcon: Icon(
-                        ManagerIcons.search,
-                        color: ManagerColors.greyPrimary,
-                      ),
-                      suffixIcon: Icon(
-                        ManagerIcons.mic,
-                        color: ManagerColors.greyPrimary,
-                      ),
+                    suffixIcon: IconService.getIcon(
+                      icon: ManagerIcons.microphone,
                     ),
                   ),
                 ),
               ),
-            ),
 
-            SliverPadding(
-              padding: EdgeInsets.only(top: ManagerHeight.h24),
-            ),
+              ///Categories
+              SliverToBoxAdapter(
+                child: Padding(
+                  padding: EdgeInsets.symmetric(vertical: ManagerHeight.h24),
+                  child: const Categories(),
+                ),
+              ),
 
-            ///Categories
-            const SliverToBoxAdapter(
-              child: Categories(),
-            ),
+              ///Articles list
+              PagedSliverList<int, Article>(
+                pagingController: controller.pagingController,
+                builderDelegate: PagedChildBuilderDelegate<Article>(
+                  animateTransitions: true,
+                  itemBuilder: (_, article, __) => Center(
+                    key: Key(article.url!),
+                    child: Padding(
+                      padding: EdgeInsets.only(bottom: ManagerHeight.h24),
+                      child: ArticleCard(
+                        imageUrl: article.imageUrl,
+                        isSaved: article.isSaved,
+                        text: article.displayText,
+                        onPressed: () => controller.onArticleCardPressed(article),
+                        onBookmarkPressed: () => controller.onBookmarkPressed(article),
+                      ),
+                    ),
+                  ),
 
-            SliverPadding(
-              padding: EdgeInsets.only(top: ManagerHeight.h24),
-            ),
-
-            ///Articles list
-            PagedSliverList<int, Article>(
-              pagingController: controller.pagingController,
-              builderDelegate: PagedChildBuilderDelegate<Article>(
-                noItemsFoundIndicatorBuilder: (context) {
-                  return Center(
+                  /// No articles
+                  noItemsFoundIndicatorBuilder: (_) => Center(
                     child: Text(
-                      'No articles found',
+                      ManagerStrings.noArticlesFound,
                       style: getSemiBoldTextStyle(
                         fontSize: ManagerFontSize.s24,
                       ),
                     ),
-                  );
-                },
-                animateTransitions: true,
-                itemBuilder: (_, article, index) => Center(
-                  key: Key(article.url!),
-                  child: Padding(
-                    padding: EdgeInsets.only(bottom: ManagerHeight.h24),
-                    child: ArticleCard(
-                      imageUrl: article.imageUrl,
-                      isSaved: article.isSaved,
-                      text: article.displayText,
-                      onPressed: () => controller.onArticleCardPressed(article),
-                      onBookmarkPressed: () => controller.onBookmarkPressed(article),
-                    ),
                   ),
+
+                  /// Loading Error
+                  firstPageErrorIndicatorBuilder: pageLoadingError,
+
+                  /// Loading Indicator
+                  firstPageProgressIndicatorBuilder: loadingArticlesIndicator,
                 ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
