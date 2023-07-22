c21ccf776b62afbc8ce6c29e5ea3e2e29ea59d50
saleem-15 <94238476+saleem-15@users.noreply.github.com>
2023-07-22 19:33:46 +0300

    Refactor: move CategoryChip to  a seperate widget

--- lib/features/category/presentation/view/categories_view.dart
+++ lib/features/category/presentation/view/categories_view.dart
@@ -1,12 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:nuntium/core/resorces/manager_colors.dart';
-import 'package:nuntium/core/resorces/manager_fonts.dart';
 import 'package:nuntium/core/resorces/manager_sizes.dart';
 import 'package:nuntium/core/resorces/manager_strings.dart';
-import 'package:nuntium/core/resorces/manager_styles.dart';
 import 'package:nuntium/core/widgets/screen_header.dart';
 import 'package:nuntium/features/category/presentation/controller/categories_controller.dart';
+
+import 'widgets/category_chip.dart';
 
 class CategoriesView extends GetView<CategoriesController> {
   const CategoriesView({super.key});
@@ -15,47 +14,38 @@ class CategoriesView extends GetView<CategoriesController> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Padding(
-          padding: EdgeInsets.symmetric(horizontal: ManagerWidth.w20),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Header(
-                title: ManagerStrings.categories,
-                paragraph: ManagerStrings.categoriesParagraph,
-                // 'Thousands of articles in each category',
-              ),
-              GetBuilder<CategoriesController>(builder: (_) {
-                return GridView.builder(
-                  shrinkWrap: true,
-                  itemCount: controller.topics.length,
-                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                      crossAxisCount: 2,
-                      crossAxisSpacing: ManagerWidth.w16,
-                      mainAxisSpacing: ManagerHeight.h16,
-                      childAspectRatio: 160 / 72),
-                  itemBuilder: (context, index) {
-                    return Container(
-                      decoration: BoxDecoration(
-                        color: ManagerColors.greyLighter,
-                        borderRadius: BorderRadius.all(
-                          Radius.circular(ManagerRadius.r12),
-                        ),
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: EdgeInsets.only(
+              right: ManagerWidth.w20,
+              left: ManagerWidth.w20,
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Header(
+                  title: ManagerStrings.categories,
+                  paragraph: ManagerStrings.categoriesParagraph,
+                ),
+                GetBuilder<CategoriesController>(
+                  builder: (_) {
+                    return GridView.builder(
+                      shrinkWrap: true,
+                      itemCount: controller.topics.length,
+                      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                        crossAxisCount: 2,
+                        crossAxisSpacing: ManagerWidth.w16,
+                        mainAxisSpacing: ManagerHeight.h36,
+                        childAspectRatio: 160 / 72,
                       ),
-                      child: Center(
-                        child: Text(
-                          controller.topics[index].name,
-                          style: getSemiBoldTextStyle(
-                            fontSize: ManagerFontSize.s16,
-                            color: ManagerColors.greyDarker,
-                          ),
-                        ),
+                      itemBuilder: (_, index) => CategoryChip(
+                        topic: controller.topics[index],
                       ),
                     );
                   },
-                );
-              }),
-            ],
+                ),
+              ],
+            ),
           ),
         ),
       ),
