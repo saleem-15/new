04dffbf9cc0634926d0434400ff19b56c620a933
saleem-15 <94238476+saleem-15@users.noreply.github.com>
2023-07-22 19:03:44 +0300

    Add: new icon size const

--- lib/core/resorces/manager_sizes.dart
+++ lib/core/resorces/manager_sizes.dart
@@ -82,6 +82,7 @@ class ManagerIconSize {
   static const double s12 = 12;
   static const double s16 = 16;
   static const double s24 = 24;
+  static const double s28 = 28;
 }
 
 class ManagerOpacity {
