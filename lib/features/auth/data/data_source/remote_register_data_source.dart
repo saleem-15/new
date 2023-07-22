dab37e6e9e38c9696e293402901f0a1f0f1dc1d6
saleem-15 <94238476+saleem-15@users.noreply.github.com>
2023-07-22 20:36:33 +0300

    Update: create user doc when register

--- lib/features/auth/data/data_source/remote_register_data_source.dart
+++ lib/features/auth/data/data_source/remote_register_data_source.dart
@@ -1,4 +1,7 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:nuntium/config/constants.dart';
+import 'package:nuntium/core/network/firestore.dart';
 import 'package:nuntium/features/auth/data/request/register_request.dart';
 
 import '../response/register_response.dart';
@@ -10,11 +13,27 @@ abstract class RemoteRegisterDataSource {
 class RemoteRegisterDataSourceImpl implements RemoteRegisterDataSource {
   @override
   Future<RegisterResponse> register(RegisterRequest registerRequest) async {
-    return RegisterResponse(
-      userCredential: await FirebaseAuth.instance.createUserWithEmailAndPassword(
-        email: registerRequest.email!,
-        password: registerRequest.password!,
-      ),
+    final userCredential = await FirebaseAuth.instance.createUserWithEmailAndPassword(
+      email: registerRequest.email,
+      password: registerRequest.password,
     );
+
+    final userDoc = await _createUserDoc(registerRequest);
+
+    return RegisterResponse(
+      userCredential: userCredential,
+      userDoc: userDoc,
+    );
+  }
+
+  Future<DocumentReference> _createUserDoc(RegisterRequest registerRequest) async {
+    final userDoc = usersCollection.doc(registerRequest.email);
+
+    await userDoc.set({
+      FirestoreConstants.name: registerRequest.name,
+      FirestoreConstants.email: registerRequest.email,
+    });
+
+    return userDoc;
   }
 }
